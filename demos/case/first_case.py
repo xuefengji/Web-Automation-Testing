9d26f5c4246ca695491c40268d2a925888b197cd
xuefengji <jxfvip@sina.com>
2019-12-16 20:41:08 +0800

    feat:po模型之case

---
+++ demos/case/first_case.py
@@ -0,0 +1,16 @@
+class FirstCase(object):
+
+    def test_user_email_error(self):
+        login('122','999')
+
+    def test_user_name_error(self):
+        login('122','999')
+
+    def test_user_password_error(self):
+        login('122','999')
+
+    def test_user_code_error(self):
+        login('122','999')
+
+    def test_user_register_success(self):
+        login('122','999')
---
+++ demos/handle/register_handle.py
@@ -0,0 +1,3 @@
+class RegisterHandle(object):
+    def send_user_email(self):
+        pass
