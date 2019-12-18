72178fcf0cf84a0dea91f5b6a4c90d2b911a7f01
xuefengji <jxfvip@sina.com>
2019-12-18 21:20:24 +0800

    feat:po模型结合unittest

--- demos/case/first_case.py
+++ demos/case/first_case.py
@@ -1,9 +1,15 @@
 from demos.business.register_business import RegisterBusiness
 from selenium import webdriver
-class FirstCase(object):
-    def __init__(self):
+import unittest
+class FirstCase(unittest.TestCase):
+    def setUp(self):
         self.driver = webdriver.Chrome()
+        self.driver.get('http://www.baidu.com')
         self.register_b = RegisterBusiness(self.driver)
+
+    def tearDown(self):
+        self.driver.close()
+
     def test_user_email_error(self):
         email_error = self.register_b.login_email_error('122','999','111111','111111')
         if email_error == True:
@@ -29,3 +35,6 @@ class FirstCase(object):
         register_success = self.register_b.login_success('122@qq.com', '999', '111111', '111111')
         if register_success == True:
             print('注册成功')
+
+if __name__=='__main__':
+    unittest.main()
