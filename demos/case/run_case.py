3768047bdd401e6f263a549b7eb941d3ec2c3913
xuefengji <jxfvip@sina.com>
2019-12-19 20:36:49 +0800

    feat:unittest大批量运行case

--- demos/case/run_case.py
+++ demos/case/run_case.py
@@ -2,10 +2,8 @@ import unittest
 import os
 class RunCase(unittest.TestCase):
     def testrun_cases(self):
-        case_dir = os.path.join(os.getcwd(),'case')
-        suite = unittest.defaultTestLoader.discover(case_dir,'unittest_*.py')
-
-        unittest.TestRunner().run(suite)
-
+        case_dir = os.path.join(os.getcwd())
+        suite = unittest.defaultTestLoader.discover(case_dir,'unittest_case*.py')
+        unittest.TextTestRunner().run(suite)
 if __name__=='__main__':
     unittest.main()
