6dbceafabc4e8c6db83842420d68b6fd66d60cc2
xuefengji <jxfvip@sina.com>
2019-12-19 20:53:21 +0800

    feat:assert使用

--- demos/case/first_case.py
+++ demos/case/first_case.py
@@ -12,29 +12,34 @@ class FirstCase(unittest.TestCase):
 
     def test_user_email_error(self):
         email_error = self.register_b.login_email_error('122','999','111111','111111')
-        if email_error == True:
-            print('注册成功，词条case失败')
+        # if email_error == True:
+        #     print('注册成功，词条case失败')
         #通过assert断言进行判断error
+        self.assertFalse(email_error)
 
     def test_user_name_error(self):
         name_error = self.register_b.login_name_error('122@qq.com', '999', '111111', '111111')
-        if name_error == True:
-            print('注册成功，词条case失败')
+        # if name_error == True:
+        #     print('注册成功，词条case失败')
+        self.assertFalse(name_error)
 
     def test_user_password_error(self):
         password_error = self.register_b.login_password_error('122@qq.com', '999', '111111', '111111')
-        if password_error == True:
-            print('注册成功，词条case失败')
+        # if password_error == True:
+        #     print('注册成功，词条case失败')
+        self.assertFalse(password_error)
 
     def test_user_code_error(self):
         code_error = self.register_b.login_code_error('122@qq.com', '999', '111111', '111111')
-        if code_error == True:
-            print('注册成功，词条case失败')
+        # if code_error == True:
+        #     print('注册成功，词条case失败')
+        self.assertFalse(code_error)
 
     def test_user_register_success(self):
         register_success = self.register_b.login_success('122@qq.com', '999', '111111', '111111')
-        if register_success == True:
-            print('注册成功')
+        # if register_success == True:
+        #     print('注册成功')
+        self.assertTrue(register_success)
 
 if __name__=='__main__':
     unittest.main()
