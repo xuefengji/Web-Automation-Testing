9421942d57fd95ddd3b89102d68d6004bcd4c9da
xuefengji <jxfvip@sina.com>
2019-12-21 21:03:35 +0800

    feat:完整case整合获取验证码

--- demos/business/register_business.py
+++ demos/business/register_business.py
@@ -3,11 +3,11 @@ class RegisterBusiness(object):
     def __init__(self,driver):
         self.register_h = RegisterHandle(driver)
 
-    def send_info(self,email,name,pwd,code):
+    def send_info(self,email,name,pwd,filename):
         self.register_h.send_user_email(email)
         self.register_h.send_user_name(name)
         self.register_h.send_user_pwd(pwd)
-        self.register_h.send_user_code(code)
+        self.register_h.send_user_code(filename)
         self.register_h.click_button()
 
 
@@ -18,32 +18,32 @@ class RegisterBusiness(object):
             return False
 
     #执行操作
-    def login_email_error(self,email,name,pwd,code):
-        self.send_info(email,name,pwd,code)
+    def login_email_error(self,email,name,pwd,filename):
+        self.send_info(email,name,pwd,filename)
         if self.register_h.get_text_elemt('email_error')==None:
             print('邮箱检验不成功')
             return True
         else:
             return False
 
-    def login_name_error(self,email,name,pwd,code):
-        self.send_info(email,name,pwd,code)
+    def login_name_error(self,email,name,pwd,filename):
+        self.send_info(email,name,pwd,filename)
         if self.register_h.get_text_elemt('name_error')==None:
             print('用户名检验不成功')
             return True
         else:
             return False
 
-    def login_password_error(self,email,name,pwd,code):
-        self.send_info(email,name,pwd,code)
+    def login_password_error(self,email,name,pwd,filename):
+        self.send_info(email,name,pwd,filename)
         if self.register_h.get_text_elemt('password_error')==None:
             print('密码检验不成功')
             return True
         else:
             return False
 
-    def login_code_error(self,email,name,pwd,code):
-        self.send_info(email,name,pwd,code)
+    def login_code_error(self,email,name,pwd,filename):
+        self.send_info(email,name,pwd,filename)
         if self.register_h.get_text_elemt('code_error')==None:
             print('验证码检验不成功')
             return True
