c5ed44e2bccfd246fd8bc8e56f13c2a581277565
xuefengji <jxfvip@sina.com>
2019-12-22 21:48:06 +0800

    feat:读取excel数据

---
+++ util/excel_util.py
@@ -0,0 +1,18 @@
+import xlrd
+
+class GetExcelData():
+    def __init__(self,path_name=None,index=None):
+        if path_name == None:
+            path_name = './../../config/excel_data.xls'
+        if index == None:
+            index = 0
+        self.excel = xlrd.open_workbook(path_name)
+        self.table = self.excel.sheet_by_index(index)
+        self.rows = self.table.nrows
+
+
+    def get_rows(self):
+        result = []
+        for i in range(self.rows):
+            data = self.table.row_values(i)
+            result.append(data)
