f33ebe7fee1e9e3d7b773a1ed3baa2ebc0499b8d
xuefengji <jxfvip@sina.com>
2019-12-23 21:20:26 +0800

    update：重新分装操作excel

--- util/excel_util.py
+++ util/excel_util.py
@@ -8,12 +8,22 @@ class GetExcelData():
             index = 0
         self.excel = xlrd.open_workbook(path_name)
         self.table = self.excel.sheet_by_index(index)
-        self.rows = self.table.nrows
 
 
     def get_rows(self):
         result = []
-        for i in range(self.rows):
+        for i in range(self.get_lines()):
             data = self.table.row_values(i)
             result.append(data)
         return result
+
+    #获取行数
+    def get_lines(self):
+        rows = self.table.nrows
+        return rows
+
+    #获取单元格数据
+    def get_col_value(self,row,col):
+        data = self.table.cell_value(row,col)
+        return data
+
