6d3a24b922ddd75e368304922d6b5edeecf87a1e
xuefengji <jxfvip@sina.com>
2019-12-25 21:43:07 +0800

    feat:获取测试结果

--- demos/case/keyword_case.py
+++ demos/case/keyword_case.py
@@ -14,15 +14,40 @@ class KeywordCase:
                     method = excel_data.get_col_value(i,4)
                     send_value = excel_data.get_col_value(i,5)
                     handel_value = excel_data.get_col_value(i,6)
+                    expact_result = excel_data.get_col_value(i,7)
+                    expact_result_value = excel_data.get_col_value(i,8)
                     # if send_value:
                     self.run_method(method,send_value,handel_value)
+                    if expact_result_value:
+                        result_value = self.get_expact_result_value(expact_result_value)
+                        if result_value[0] == 'text':
+                            value = self.run_method(expact_result)
+                            if result_value[1] in value:
+                                excel_data.write_data(i,'pass')
+                            else:
+                                excel_data.write_data(i,'fail')
+                        elif result_value[0] == 'element':
+                            value = self.run_method(expact_result,result_value[1])
+                            if value:
+                                excel_data.write_data(i,'pass')
+                            else:
+                                excel_data.write_data(i,'fail')
+                        else:
+                            print('预期结果为空')
 
-    def run_method(self,method,send_value,handel_value):
-
+    def run_method(self,method,send_value='',handel_value=''):
         method_value = getattr(self.action_method,method)
-        if send_value:
-            method_value(send_value,handel_value)
-        elif send_value == '' and handel_value != '':
-            method_value(handel_value)
+        if send_value == '' and handel_value != '':
+            result = method_value(handel_value)
+        elif send_value != '' and handel_value == '':
+            result = method_value(send_value)
+        elif send_value != '' and handel_value != '':
+            result = method_value(send_value,handel_value)
         else:
-            method_value()
+            result = method_value()
+        return result
+
+    #获取预期结果值
+    def get_expact_result_value(self,data):
+        result_value = data.split('=')
+        return result_value
