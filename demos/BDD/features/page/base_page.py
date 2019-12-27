9a54f3e64e2edadc4c33e4ec82988cde80be9fca
xuefengji <jxfvip@sina.com>
2019-12-27 21:33:32 +0800

    feat:行为驱动代码与pageobject结合

--- demos/BDD/features/page/base_page.py
+++ demos/BDD/features/page/base_page.py
@@ -1,5 +1,17 @@
+
 class BasePage:
     def __init__(self,driver):
         self.driver = driver
-    def find_element(self):
-        self.driver.find_element_by_id()
+
+    #打开页面
+    def get_url(self,url):
+        self.driver.get(url)
+
+    #获取title
+    def get_title(self):
+        return self.driver.title
+
+
+    #定位元素
+    def find_element(self,*loc):
+        return self.driver.find_element(*loc)
