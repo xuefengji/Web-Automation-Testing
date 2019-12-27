d08478cca396fdb5c84416a5e9863558d3e21eff
xuefengji <jxfvip@sina.com>
2019-12-27 20:50:53 +0800

    feat:行为驱动代码

--- demos/BDD/features/steps/register_user.py
+++ demos/BDD/features/steps/register_user.py
@@ -13,3 +13,25 @@ def step_register(context,title_name):
 @when('I set with useremail "([^"]*)"')
 def step_register(context,useremail):
     context.driver.find_element_by_id('register_email').send_keys(useremail)
+
+@when('I set with username "([^"]*)"')
+def step_register(context,username):
+    context.driver.find_element_by_id('register_nickname').send_keys(username)
+
+@when('I set with password "([^"]*)"')
+def step_register(context,password):
+    context.driver.find_element_by_id('register_password').send_keys(password)
+
+@when('I set with code "([^"]*)"')
+def step_register(context,code):
+    context.driver.find_element_by_id('register_code').send_keys(code)
+
+@when('I click with registerbutton')
+def step_register(context):
+    context.driver.find_element_by_id('register_btn').click()
+
+
+@then('I expect that text "([^"]*)"')
+def step_register(context,code_text):
+    text = context.driver.find_element_by_id('register_code_text').text()
+    assert code_text in text
