70b1fa97ccc51d1d600eb8da46f012fdf8842c8b
xuefengji <jxfvip@sina.com>
2019-12-28 19:49:22 +0800

    feat:add log output file

--- demos/log/user_log.py
+++ demos/log/user_log.py
@@ -1,8 +1,17 @@
 import logging
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)   #设置log等级
-consle = logging.StreamHandler()
-logger.addHandler(consle)
+
+#输出日志
+# consle = logging.StreamHandler()
+# logger.addHandler(consle)
+
+#写入log文件
+file_consle = logging.FileHandler('./logs/test.log')
+logger.addHandler(file_consle)
+
 logger.debug("test")
-consle.close()
-logger.removeHandler(consle)
+# consle.close()
+# logger.removeHandler(consle)
+file_consle.close()
+logger.removeHandler(file_consle)
