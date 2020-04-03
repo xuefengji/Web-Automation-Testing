8e3acc6a5b68fb4fdad4fbacc611862a51dd1028
xuefengji <jxfvip@sina.com>
2020-04-03 22:04:31 +0800

    docs: update

--- demos/case/run_case.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import os
-class RunCase(unittest.TestCase):
-    def testrun_cases(self):
-        case_dir = os.path.join(os.getcwd())
-        suite = unittest.defaultTestLoader.discover(case_dir,'unittest_case*.py')
-        unittest.TextTestRunner().run(suite)
-if __name__=='__main__':
-    unittest.main()
