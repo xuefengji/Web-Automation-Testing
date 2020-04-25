e2104f82d2044df2b478e018f4cffd0c2d45acdc
xuefengji <jxfvip@sina.com>
2020-04-25 20:53:25 +0800

    docs: update

--- WX Auto/demo/indexDemo/test.js
+++ WX Auto/demo/indexDemo/test.js
@@ -1,13 +1,8 @@
 const automator = require('miniprogram-automator');
 
-const miniProgram = automator.connect({
-  wsEndpoint: 'ws://localhost:15540'
+automator.connect({
+  wsEndpoint: 'ws://localhost:55536'
 }).then(async miniProgram => {
-  // 从首页重启
-  const page = await miniProgram.reLaunch('/pages/index/index');
+  const page = await miniProgram.navigateTo('/page/index/index')
   await page.setData({})
-  // // 从页面获取bottom-button组件
-  // const button = await page.$('bottom-button');
-  // // 打印出button的wxml信息
-  // console.log(await button.wxml());
 })
