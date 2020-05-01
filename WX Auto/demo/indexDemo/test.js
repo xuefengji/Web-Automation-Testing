cfab8b3ca0a1ef620f3e79808759b25c20232723
xuefengji <jxfvip@sina.com>
2020-05-01 19:51:12 +0800

    docs: update

--- WX Auto/demo/indexDemo/test.js
+++
@@ -1,8 +0,0 @@
-const automator = require('miniprogram-automator');
-
-automator.connect({
-  wsEndpoint: 'ws://localhost:55536'
-}).then(async miniProgram => {
-  const page = await miniProgram.navigateTo('/page/index/index')
-  await page.setData({})
-})
