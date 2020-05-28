1b5424ec886a9b0ceeb2217351749438207aacc1
@xuefeng.ji <jxfvip@sina.com>
2020-05-28 11:41:32 +0800

    docs: add

---
+++ WX Auto/demo/indexDemo/main.test.js
@@ -0,0 +1,24 @@
+const automator = require('miniprogram-automator')
+
+describe('index', () => {
+  let miniProgram
+  let page
+
+  beforeAll(async () => {
+    miniProgram = await automator.launch({
+        cliPath: 'E:\\微信web开发者工具\\cli.bat',
+        projectPath: 'F:\\simlove-wechat-mini'
+    })
+    page = await miniProgram.reLaunch('/pages/index/index')
+    await page.waitFor(500)
+  }, 30000)
+
+  test('测试',()=>{
+    expect('hello').toMatch('he')
+  })
+
+  
+//   afterAll(async () => {
+//     await miniProgram.close()
+//   })
+})
