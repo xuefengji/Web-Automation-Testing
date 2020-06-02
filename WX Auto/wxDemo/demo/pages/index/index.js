fb68b5ad5f59e5cec1639d10f98fbd9e19d011f7
xuefengji <jxfvip@sina.com>
2020-06-02 22:30:56 +0800

    docs: update

--- WX Auto/wxDemo/demo/pages/index/index.js
+++ WX Auto/wxDemo/demo/pages/index/index.js
@@ -1,13 +1,13 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    time: (new Date()).toString()
   },
   //事件处理函数
   bindViewTap: function() {
