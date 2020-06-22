c22c2a68f2631e2a63db61fd2a32f45182542c67
xuefengji <jxfvip@sina.com>
2020-06-22 21:05:04 +0800

    docs: update

--- WX Auto/wxDemo/demo/pages/index/index.js
+++ WX Auto/wxDemo/demo/pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 Page({
   data: {
     motto: 'Hello World',
-    array:'APP',
+    // array:'APP',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
