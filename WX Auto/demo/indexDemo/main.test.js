4e50dcf20193526196fd7f5acb6a260fa821369a
xuefengji <jxfvip@sina.com>
2020-07-12 21:02:57 +0800

    docs: update

--- WX Auto/demo/indexDemo/main.test.js
+++ WX Auto/demo/indexDemo/main.test.js
@@ -1,11 +1,11 @@
 const automator = require('miniprogram-automator')
+// const goodsBuyTest = require('./testCase/goodBuy.test')
 // const opreate = require('./utils/opreate.js')
 // const {getElement} = opreate
 
 describe('index', () => {
   let miniProgram
   let page
-  let elements
 
   beforeAll(async () => {
     miniProgram = await automator.launch({
@@ -22,29 +22,20 @@ describe('index', () => {
     // console.log(page)
   }, 30000)
 
-  test('购买商品', async () => {
-    //  elements = await page.$$('.classify')
-    elements = await page.$$('.goods-item')
-    console.log(elements)
-    // clickButton = await elements[0].$('van-stepper-54c9e8')
-    //  console.log(elements[0].id)
-    // expect(page.path).toMatch('pages/index/index');
-    // var element = await elements[0].$('view')
-    console.log(element.length)
-    await element[0].tap()
-  //  var id = elements[0].id
-  //   page = await miniProgram.navigateTo('/pages/goodsPage/goodsPresellDetail/index?id=' + id);
+  test('购买商品', async (page) =>{
+    //获取商品系列中的商品
+    let goodItems = await page.$$('.goods-item')
+    console.log(goodItems.length)
+    let tapElements = await goodItems[0].$$('view')
+    await tapElements[0].tap()
+    // var id = elements[0].id
+    // page = await miniProgram.navigateTo('/pages/goodsPage/goodsPresellDetail/index?id=' + id);
     page = await miniProgram.currentPage()
     console.log(page.path)
     console.log(await miniProgram.pageStack())
-    // console.log(aa)
-    // const element = await elements[0].$$('')
-    // console.log(element)
-    // await element.tap()
-    // console.log(await miniProgram.currentPage())
   })
 
-  // afterAll(async () => {
-  //   await miniProgram.close()
-  // })
+  afterAll(async () => {
+    await miniProgram.close()
+  })
 })
