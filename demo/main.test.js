e14324c4d5291dc7b642931590ba2352c2242b6c
xuefengji <xuefvip@163.com>
2020-11-26 22:45:03 +0800

    add: demo/main.test.js

---
+++ demo/main.test.js
@@ -0,0 +1,10 @@
+const automator = require('miniprogram-automator')
+
+automator.launch({
+  cliPath: 'D:\\Program Files (x86)\\Tencent\\微信web开发者工具\\cli.bat', // 工具 cli 位置，如果你没有更改过默认安装位置，可以忽略此项
+  projectPath: 'E:\\work\\simlove-wechat-mini', // 项目文件地址
+}).then(async miniProgram => {
+  const page = await miniProgram.reLaunch('/page/index/index')
+  await page.waitFor(3000)
+ 
+})
