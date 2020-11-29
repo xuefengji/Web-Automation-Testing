1daff5afa0ab6374aeeed333ce05c02250fc81bc
xuefengji <xuefvip@163.com>
2020-11-29 14:48:09 +0800

    add: update

--- demo/main.test.js
+++ demo/main.test.js
@@ -1,18 +1,25 @@
-const { TestScheduler } = require('jest')
 const automator = require('miniprogram-automator')
 
-test('index',()=>{
-  automator.launch({
-    cliPath: 'D:\\Program Files (x86)\\Tencent\\微信web开发者工具\\cli.bat', // 工具 cli 位置，如果你没有更改过默认安装位置，可以忽略此项
-    projectPath: 'E:\\work\\simlove-wechat-mini', // 项目文件地址
-  }).then(async miniProgram => {
-    const page = await miniProgram.redirectTo('/pages/goodsPage/goodsLimitDetail/index?id=44&activityType=RESTRITED&activityId=256&presellFlag=true')
-    await page.waitFor(async () => {
-      return (await miniProgram.pageStack().length) > 0})
-    // const pageStacks = await miniProgram.pageStack()
-  
-   
-   
+describe('index', () => {
+  let miniProgram
+  let page
+
+  beforeAll(async () => {
+    miniProgram = await automator.launch({
+      cliPath: 'D:\\Program Files (x86)\\Tencent\\微信web开发者工具\\cli.bat', // 工具 cli 位置，如果你没有更改过默认安装位置，可以忽略此项
+      projectPath: 'E:\\work\\simlove-wechat-mini', // 项目文件地址
+    })
+    page = await miniProgram.reLaunch('/pages/component/index')
+    await page.waitFor(500)
+  }, 30000)
+
+  afterAll(async () => {
+    await miniProgram.close()
+  })
+
+  it('index', ()=>{
+    
   })
 })
+
 
