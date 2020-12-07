c3ea7a47e90745afd98822a5736e96c6246f2883
xuefengji <xuefvip@163.com>
2020-12-07 21:57:12 +0800

    docs: Add comments

--- demo/utils/singleBuy.js
+++ demo/utils/singleBuy.js
@@ -12,30 +12,30 @@ async function singleBuy(miniProgram){
         const address = await detailpage.$('.bar-index--location-name')
     // console.log(await address.wxml())
     // const addressText = await address.$('.location-name')
-    console.log(await address.text())
-    if(!await address.text()){
-    console.log(2)
-    const addressSelect = await detailpage.$('.bar-index--user-location-content')
-    console.log(await addressSelect)
-    console.log(3)
-    await addressSelect.tap()
-    await detailpage.waitFor(3000)
-    const add = await detailpage.$$('.bar-index--date-item')
-    await add[0].tap()
-    await detailpage.waitFor(3000)
-    
-    }
-    //加入购物车并点击单次购买按钮
-    const add2cart = await detailpage.$('.add2cart')
-    
-    if(await add2cart.text() === '单次购买'){
-    await detailpage.waitFor(3000)
-    await add2cart.tap()
-    await detailpage.waitFor(3000)
-    
-    const paybtn = await detailpage.$('.cart-index--pay-btn')
-    await paybtn.tap()
-    await detailpage.waitFor(3000)
+        console.log(await address.text())
+        if(!await address.text()){
+        console.log(2)
+        const addressSelect = await detailpage.$('.bar-index--user-location-content')
+        console.log(await addressSelect)
+        console.log(3)
+        await addressSelect.tap()
+        await detailpage.waitFor(3000)
+        const add = await detailpage.$$('.bar-index--date-item')
+        await add[0].tap()
+        await detailpage.waitFor(3000)
+        
+        }
+        //加入购物车并点击单次购买按钮
+        const add2cart = await detailpage.$('.add2cart')
+        
+        if(await add2cart.text() === '单次购买'){
+        await detailpage.waitFor(3000)
+        await add2cart.tap()
+        await detailpage.waitFor(3000)
+        
+        const paybtn = await detailpage.$('.cart-index--pay-btn')
+        await paybtn.tap()
+        await detailpage.waitFor(3000)
     
     }
     }catch(e){
