6818fe16c3be49aafe4b6a0f346ad0c940f469f6
xuefengji <xuefvip@163.com>
2020-12-10 21:31:23 +0800

    feat: add get the user address

--- demo/utils/orderPay.js
+++ demo/utils/orderPay.js
@@ -3,14 +3,17 @@
  * describe: 从订单结算页到支付页
  */
 const address = require('../api/address')
- async function orderPay(miniProgram){
+const getAddress = require('../api/getAddress')
+async function orderPay(miniProgram){
     const orderPage = await miniProgram.currentPage()
     console.log(orderPage.path)
     //判断是否有默认地址
     const noAddress = await orderPage.$('.no-address')
     if(noAddress){
-        address()
+        await getAddress()
+        await noAddress.tap()
     }
+     
  }
 
 
