77544d77e4530fe988b28ed2fe0f250c731314e2
xuefengji <xuefvip@163.com>
2020-12-12 10:58:43 +0800

    refactor: modify file name

--- demo/testCases/index.js
+++ demo/testCases/index.js
@@ -2,9 +2,9 @@
 author: xuefengji
 describe: 从首页进入单次商品详情页，再从商品详情页进入定单结算页
 */
-const getOneProduct = require('../utils/getProduct')
-const singleBuy = require('../utils/singleBuy')
-const orderPay = require('../utils/orderPay')
+const getOneProduct = require('../handel/getProduct')
+const singleBuy = require('../handel/singleBuy')
+const orderPay = require('../handel/orderPay')
 async function indexToOrder(miniProgram){
      //获取首页或全部商品页的任意商品
       await getOneProduct(miniProgram)
