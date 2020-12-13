b70734ae8ccdb1193be18e9b0b5d199ac2f447a8
xuefengji <xuefvip@163.com>
2020-12-13 19:18:19 +0800

    feat: go to the payment page

--- demo/handel/getProduct.js
+++ demo/handel/getProduct.js
@@ -2,7 +2,7 @@
  * author: xuefengji
  * describe: 获取单次商品并进入详情页
  */
-
+const ranNum = require('../utils/getRandomNum')
 async function getOneProduct(miniProgram){
     const page = await miniProgram.currentPage();
     console.log(await page.path);
@@ -13,6 +13,10 @@ async function getOneProduct(miniProgram){
       const goodItems = await page.$$('.goods-item');
       console.log(goodItems.length)
       // console.log(elements[0])
+      //随机获取商品
+      // const tapElements = await goodItems[ranNum(goodItems.length)].$$('view');
+      // await tapElements[ranNum(tapElements.length)].tap()
+      //为方便测试环境测试，使用一个商品
       const tapElements = await goodItems[2].$$('view');
       await tapElements[0].tap()
       // console.log(await miniProgram.pageStack())
--- demo/handel/orderPay.js
+++ demo/handel/orderPay.js
@@ -8,20 +8,37 @@ const ranNum = require('../utils/getRandomNum')
 async function orderPay(miniProgram){
     const orderPage = await miniProgram.currentPage()
     console.log(orderPage.path)
-    //判断是否有默认地址
-    const noAddress = await orderPage.$('.no-address')
-    if(noAddress){
-        await getAddress()
-        await noAddress.tap()
-        await orderPage.waitFor(30000)
-        const selectAddress = await miniProgram.currentPage()
-        console.log(selectAddress.path)
-        const addDetail = await selectAddress.$$('.add-detail-top')
-        console.log(await addDetail.length)
-        await addDetail[ranNum(addDetail.length)].tap()
+    try{
+        const noAddress = await orderPage.$('.no-address')
+        //判断是否有默认地址
+        if(noAddress){
+            await getAddress()
+            await noAddress.tap()
+            await orderPage.waitFor(30000)
+            const selectAddress = await miniProgram.currentPage()
+            console.log(selectAddress.path)
+            const addDetail = await selectAddress.$$('.add-detail-top')
+            console.log(await addDetail.length)
+            await addDetail[ranNum(addDetail.length)].tap()
+            await selectAddress.waitFor(3000)
+        }
+        const dateTime = await orderPage.$$('.opera-item')
+        await dateTime[1].tap()
+        await orderPage.waitFor(300)
+        //判断是否有库存
+        if(await orderPage.$('.date-item')){
+            const dateSelect = await orderPage.$$('.date-item')
+            await dateSelect[ranNum(dateSelect.length)].tap()
+            await orderPage.waitFor(300)
+            const payButton = await orderPage.$('.pay-button')
+            await payButton.tap()
+        }
+        console.log('当前无库存')
+    }catch(e){
+
     }
     
-    
+
  }
 
 
